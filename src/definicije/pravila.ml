d1abd8c2885de8177772f6a8b7bafff9666e21c0
Lovro Drofenik <ld7426@student.uni-lj.si>
2024-09-06 00:24:29 +0200

    urejena datoteka pravila.ml

--- src/definicije/pravila.ml
+++ src/definicije/pravila.ml
@@ -1,8 +1,10 @@
 (* Datoteka s pravili za spremembo stanj *)
-(*želim funkcijo, ki vzame matriko dobljeno z izločeno in na podlagi pravila določi ali bo nov element živ ali ne, mogoče modificiram matriko s številom sosedov, da vključuje podatek, ali je kvaddratek sam živ ali mrtev*)
-(*ideja za pravila je npr urejen seznam mej med tem, ali so živi ali mrtvi*)
-(*druga ideja je samo seznam vseh vrednosti, za katere ostane živ*)
+(*Imamo matriko sosedov, ki nam definira, katere sosede vzamemo, nato preštejemo koliko od teh je živih in imamo
+shranjeno kaj se zgodi s celico glede na to, kakšno stanje je imela trenutno in koliko sosedov je živih*)
 
+
+(*########################################################################################################*)
+(*Pomožne funkcije brez posebne teorije, dost self-explanatory*)
 let rec randomarray n =
   match n with
   | 0 -> [| |]
@@ -12,6 +14,16 @@ let rec randommatrika m n =
   match m with 
   | 0 -> [| |]
   | k -> Array.append [|randomarray n|] (randommatrika (k-1) n)
+
+
+let init_matrix rows cols f = (* ta funkcija bi mogla bit že vključena v Array, pa ni... *)
+Array.init rows (fun i -> Array.init cols (fun j -> f i j))
+
+let intofbool = function
+| true -> 1
+| false -> 0
+
+let mojmod x m = ((x mod m)+m) mod m (*če je negativno nam da iz druge strani m-ja, ker drugače nam da -2 mod 3 = -2, zdaj je pa 1*)
 
 let (^^^) x y = x ^ " " ^ y
 
@@ -26,26 +38,21 @@ let izpisilistint listint = (*želim da izpiše list, ne pa array*)
   List.iter (fun x -> print_int x; print_string " :: ") listint;
   print_string "[]"
 
-let rec notri element mnozica=
+(*let rec notri element mnozica=
   match mnozica with (*sem se hotel izogniti temu, da preverja do konca ampak ne vem kako bi to naredil efficiently, hopefully ima ocaml že implementirano,
   da je    true || (KARKOLI) = true    kar pa malo dvomim...    ..., načeloma bodo množice relativno majhne, tako da to ne bo vplivalo preveč na performance*)
   |h::t -> (h==element) || (notri element t)
-  |[] -> false
+  |[] -> false*)
 
-let pravilo (trenutnostanje,stsosed) (pravilazivi, pravilamrtvi)= 
+
+(*########################################################################################################*)
+(*Glavne funkcije, ki definirajo pravilo*)
+let pravilo (trenutnostanje,stsosed) (pravilazivi, pravilamrtvi) = 
 (*iz trenutnega stanja in pravil, ki se ločijo glede na trenutno stanje določi ali bo v naslednji iteraciji živ*)
   match trenutnostanje with
-  |true -> notri stsosed pravilazivi
-  |false -> notri stsosed pravilamrtvi
+  |true -> List.mem stsosed pravilazivi
+  |false -> List.mem stsosed pravilamrtvi
 
-let init_matrix rows cols f = (* ta funkcija bi mogla bit že vključena v Array, pa ni... POZOR!!!funkcija definirana tukaj in v izlocisosede *)
-  Array.init rows (fun i -> Array.init cols (fun j -> f i j))
-
-let intofbool = function
-  | true -> 1
-  | false -> 0
-
-let mojmod x m = ((x mod m)+m) mod m (*če je negativno nam da iz druge strani m-ja, ker drugače nam da -2 mod 3 = -2, zdaj je pa 1*)
 
 let izlocivsotososedov celamatrika matrikasosescine k prviindeks drugiindeks =
   (*funkcija prešteje število sosedov celice na mestu prviindeks drugiindeks*)
@@ -71,7 +78,9 @@ let poenotenkorak matrika sosedi pravila =
   let k = Array.length sosedi in  
   init_matrix m n (fun i j -> pravilo (matrika.(i).(j), (izlocivsotososedov matrika sosedi k i j)) pravila)
 
-(*conway pravila*)
+
+(*########################################################################################################*)
+(*začetna conway pravila*)
 let zacetnapravila = ([2;3], [3]) (*Conway*)
 let zacetnik = 3
 let zacetnisosedi = [|[|true; true; true|]; [|true; false; true|]; [|true; true; true|]|]
